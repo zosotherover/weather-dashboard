9a78a7b63e217e16a06b8ba00662a12a68263bab
zosotherover <bryanpero@gmail.com>
2020-05-17 18:01:30 -0400

    worked on JS/html

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-/*
+
 
 var cityWeather;
 if (localStorage.getItem("cities")) {
@@ -15,7 +15,7 @@ for (var i = 0; i < cityWeather.length; i++) {
   $(".search").append(a);
 }
 
-function searchOneDayWeather(name) {
+function searchCurrentWeather(name) {
 
   console.log(name);
  
@@ -27,4 +27,46 @@ function searchOneDayWeather(name) {
     cityWeather.push(name);
     localStorage.setItem("cities", JSON.stringify(cityWeather));
   }
-  */
+  
+
+ $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    //Function to get the current weather via current weather data call
+    console.log(response);
+    var city = response.name;
+    var temp = response.main.temp;
+    var humidity = response.main.humidity;
+    var windSpeed = response.wind.speed;
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+    console.log(lat, lon);
+
+//  generate the city, date, temperature, humidity, wind speed
+    // generate weather div
+
+    var bodyEl = $(".currentWeather");
+    var forecastEl = $("#forecastDiv");
+    //clear clearing element!
+    bodyEl.html("");
+    forecastEl.html("");
+    var weatherDiv = $("<div class='weather'>");
+    bodyEl.append(weatherDiv);
+    var pOne = $("<p>").text("City: " + city);
+    pOne.appendTo(weatherDiv);
+    var pTwo = $("<p>").text("Temperature: " + temp);
+    pTwo.appendTo(weatherDiv);
+    var pThree = $("<p>").text("Humidity: " + humidity);
+    pThree.appendTo(weatherDiv);
+    var pFour = $("<p>").text("Windspeed: " + windSpeed);
+    pFour.appendTo(weatherDiv);
+    var timeSteup = moment().format("MMM Do, YYYY");
+    var pFive = $("<p>").text(timeSteup);
+    pFive.appendTo(weatherDiv);
+    weatherDiv.appendTo(forecastEl);
+    //UV Index: 
+    getUVIndex(lat, lon);
+    fiveDayForecast(name);
+  });
+}
